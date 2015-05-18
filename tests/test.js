c4da3a41cabf745f0d4f3da9c64b2d66527240b1
Simon Schick <simon.schick@omnea.de>
2015-05-18 17:31:56 +0200

    Added tests

---
+++ tests/test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var ArgumentParser = require('../index');
+
+var parser = new ArgumentParser('Xing clickbot that visits profiles based on keywords', {
+	ab: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg',
+	},
+	cd: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg',
+	},
+	ef: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg'
+	},
+	gh: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg'
+	},
+	ij: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg'
+	},
+	kl: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg'
+	},
+	mn: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg'
+	},
+	op: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg'
+	},
+	qr: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg'
+	},
+	st: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg'
+	},
+	uv: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg',
+	},
+	xyz: {
+		type:			'string',
+		description:	'aioghsdioughsdoigdpfgodjgodipfg'
+	}
+});
+
+var config = parser.run();
